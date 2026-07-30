22657aa76a078918263b2a9b1e6acad5e262e20b
mirayinci <156175093+mirayinci@users.noreply.github.com>
2026-07-30 15:43:33 +0300

    a

--- rezervasyon_frontend/src/app/(hakkimizda)/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function HakkimizdaLayout({ children }: { children: React.ReactNode }) {
-    return (
-        <div className="flex flex-col min-h-screen bg-slate-50">
-            {children}
-        </div>
-    );
-}
