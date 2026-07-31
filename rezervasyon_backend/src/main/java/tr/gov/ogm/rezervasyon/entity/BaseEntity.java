37d240cf28f3afcb37cb8fab6617543bead79529
Beyza Yıldırım <beyza.yildirim9@icloud.com>
2026-07-31 11:36:39 +0300

    Update BaseEntity.java

--- rezervasyon_backend/src/main/java/tr/gov/ogm/rezervasyon/entity/BaseEntity.java
+++ rezervasyon_backend/src/main/java/tr/gov/ogm/rezervasyon/entity/BaseEntity.java
@@ -1,4 +1,42 @@
 package tr.gov.ogm.rezervasyon.entity;
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDateTime;
 
+@Getter
+@Setter
+@MappedSuperclass
 public class BaseEntity {
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "created_by", updatable = false)
+    private String createdBy;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "is_deleted")
+    private boolean isDeleted=false;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+    public LocalDateTime getCreatedAt() {
+        return this.createdAt;
+    }
+
+    public void setDeleted(boolean deleted) {
+        this.isDeleted = deleted;
+    }
 }
