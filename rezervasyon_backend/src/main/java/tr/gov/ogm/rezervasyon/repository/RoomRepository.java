48169727b56f06400fcc635635308db540e16c59
Beyza Yıldırım <beyza.yildirim9@icloud.com>
2026-07-31 11:37:43 +0300

    Update RoomRepository.java

--- rezervasyon_backend/src/main/java/tr/gov/ogm/rezervasyon/repository/RoomRepository.java
+++ rezervasyon_backend/src/main/java/tr/gov/ogm/rezervasyon/repository/RoomRepository.java
@@ -1,4 +1,9 @@
 package tr.gov.ogm.rezervasyon.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import tr.gov.ogm.rezervasyon.entity.Room;
 
-public interface RoomRepository {
+@Repository
+public interface RoomRepository extends JpaRepository<Room, Long> {
+    boolean existsRoomByRoomType(String roomType);
 }
